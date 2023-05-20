bae9347d39aeb01daa258e14fae6e26c38ec0554
N SAIKRISHNA REDDY <saikrishnareddy.nagulapally2@gmail.com>
2023-05-20 16:29:25 +0530

    angular

---
+++ myapp/src/app/all-user-orders/all-user-orders.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-user-orders',
+  templateUrl: './all-user-orders.component.html',
+  styleUrls: ['./all-user-orders.component.css']
+})
+export class AllUserOrdersComponent {
+
+}
